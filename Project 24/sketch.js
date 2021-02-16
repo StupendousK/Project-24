e87a0de5fdfd6431e1b9d21ddf970755e5c23f87
StupendousK <69280404+StupendousK@users.noreply.github.com>
2021-02-16 13:01:14 -0500

    Add files via upload

---
+++ Project 24/sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+
+var dustbin;
+
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	paper = new Paper(50, 615, 20);
+
+	dustbin = new Dustbin(1000, 630);
+	ground = new Ground(800, 650, 1600, 30);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+
+
+  paper.display();
+  dustbin.display();
+  ground.display();
+
+  drawSprites();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		
+		Matter.Body.applyForce(paper.body, paper.body.position,{x:85, y:-85});
+
+	}
+}
+
+
+
